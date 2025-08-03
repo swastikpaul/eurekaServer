302b1a09a1a33797c56d92018b0fc343c21a8c65
vishwanathpal <vishwanath15@hotmail.com>
2025-08-04 04:29:51 +0530

    eurka server

---
+++ src/main/java/com/eureka/server/configuration/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.eureka.server.configuration;
+
+//@EnableWebSecurity
+//@Configuration
+public class WebSecurityConfig<SecurityFilterChain> {
+
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//		httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+//		return httpSecurity.build();
+//
+//	}
+}
